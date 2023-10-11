0f9e495ab45e5f6957fc2bb81617282e3afa5ca0
Master-Victor <vvillafane@frba.utn.edu.ar>
2023-10-11 17:48:28 -0300

    clase-16

---
+++ clase-16/api/routes/route.api.productos.js
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import * as controllers from '../controllers/controller.api.productos.js'
+import routeOpinion from './route.api.producto_opinion.js'
+import { validateProducto, validateProductoPatch } from '../../middleware/producto.validate.middleware.js'
+
+const route = Router()
+
+/*function todos(req, res, next) {
+    console.log("tengo un rol valido" )
+    next()
+}
+route.use('/productos'. todos)*/
+//traer todos los productos
+route.get('/productos',controllers.getProductos)
+//traer un producto especifico
+route.get('/productos/:id', controllers.getProductoById)
+
+route.all('/productos/:id',function todos(req, res, next) {
+    console.log("tengo un rol valido" )
+    next()
+})
+
+//agregar un producto
+route.post('/productos',[validateProducto], controllers.agregarProducto)
+//remplazar un producto -> PUT -> 
+route.put('/productos/:id', controllers.remplazarProducto)
+//actualizar un producto -> PATCH ->
+route.patch('/productos/:id',[validateProductoPatch], controllers.actualizarProducto)
+//eliminar un producto
+route.delete("/productos/:id", controllers.eliminarProducto)
+
+route.use(routeOpinion)
+
+export default route
